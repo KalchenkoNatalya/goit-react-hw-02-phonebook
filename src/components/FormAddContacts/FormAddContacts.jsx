08dd5ae9bda2de0299b68eac09e149ef458cf220
KalchenkoNatalya <janatka2222@gmail.com>
2023-06-28 21:20:41 +0300

    pattern

--- src/components/FormAddContacts/FormAddContacts.jsx
+++ src/components/FormAddContacts/FormAddContacts.jsx
@@ -34,7 +34,7 @@ export class FormAddContacts extends Component {
           type="text"
           name="name"
           value={this.state.name}
-          pattern="^[a-zA-Zа-яА-Я]+([' -][a-zA-Zа-яА-Я ]+)*$"
+          pattern="^[A-Za-z\u0080-\uFFFF ']+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           id={this.idNameInput}
           onChange={this.inputChange}
@@ -45,7 +45,7 @@ export class FormAddContacts extends Component {
           type="tel"
           name="number"
           value={this.state.number}
-          pattern="^[a-zA-Zа-яА-Я]+([' -][a-zA-Zа-яА-Я ]+)*$"
+          pattern="^(\+?[0-9.\(\)\-\s]*)$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           id={this.idNumberInput}
           onChange={this.inputChange}
